5ba394aac206ebcb0d8c0115c3171350faa93abf
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-23 21:07:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 
 resource "aws_vpc" "main" {                            
   cidr_block = "10.0.0.0/16"
-  enable_dns_support   = true 
+  enable_dns_support   = true
   enable_dns_hostnames = true   
 
   tags = {
