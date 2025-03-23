417a559b32ef26c36bf98c109c4d5b342c0555b4
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-23 22:58:48 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,3 +7,33 @@ variable "egress-rules" {
   type = list(number)
   default = [22,8080,80,443,2049]
 }
+
+variable "server_configs" {        
+  description = "A map of server configurations"                                                 
+  type = map(object({
+    name          = string
+    ami           = string
+    keyname       = string
+    ami           = sting
+    instance_type = string
+    availability_zone = string 
+  
+  }))
+  default = {
+    "web_server" = {
+      name          = "instance-1"
+      ami           = "ami-12345678"       
+      instance_type = "t2.micro"
+      key_name       = "remote" 
+      availability_zone = "us-east-1a"   
+    }
+ 
+   "web_server" = {
+      name          = "instance-2" 
+      ami           = "ami-12345678"       
+      instance_type = "t2.micro"
+      key_name       = "remote" 
+      availability_zone = "us-east-1b"
+    }
+  }
+}
