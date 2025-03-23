668ac4610893259ba20e934ed205d9eaffdb9e60
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-23 23:03:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -101,35 +101,6 @@ resource "aws_security_group" "efs-sg" {
   }
 }	
 
-variable "server_configs" {        
-  description = "A map of server configurations"                                                 
-  type = map(object({
-    name          = string
-    ami           = string
-    keyname       = string
-    ami           = sting
-    instance_type = string
-    availability_zone = string 
-  
-  }))
-  default = {
-    "web_server" = {
-      name          = "instance-1"
-      ami           = "ami-12345678"       
-      instance_type = "t2.micro"
-      key_name       = "splunk" 
-      availability_zone = "ap-south-1a"   
-    }
- 
-   "web_server" = {
-      name          = "instance-2" 
-      ami           = "ami-12345678"       
-      instance_type = "t2.micro"
-      key_name       = "splunk" 
-      availability_zone = "ap-south-1b"
-    }
- }
-}
 
 resource "aws_instance" "server" {  
   for_each = var.server_configs
