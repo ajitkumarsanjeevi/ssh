657900804e508f61165365e21be9a269c134e3f4
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-24 20:35:02 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -40,7 +40,7 @@ echo "notfound"
   fi 
 
 
-sudo mkdir /efs
+sudo mkdir $path
 
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $fsid.efs.us-east-1.amazonaws.com:/ $path
 
