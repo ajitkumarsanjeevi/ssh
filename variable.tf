f93f23968719da6986d55efc17c97ae879f8030d
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-24 08:29:11 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -8,14 +8,13 @@ variable "egress-rules" {
   default = [22,8080,80,443,2049]
 }
 variable "ec2_instance_config" {
-  description = "Map of EC2 instance configurations"
-  type = map(any)  # This allows any value type in the map
+  type = map(any)  
   default = {
     "web" = {
       instance_type = "t2.micro"
-      ami           = "ami-0c55b159cbfafe1f0"  
+      ami           =  "ami-08b5b3a93ed654d19"
       key_name      = "remote"
-      availabilityzone = "us-east-1a
+      availability_zone = "us-east-1a
     }
   }
 }
