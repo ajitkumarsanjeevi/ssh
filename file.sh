ee4d3a4f4e62a0b6272b57756b696eb97e568215
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-24 23:20:28 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -2,15 +2,8 @@
                                                                             
   
 ID=$(cat /etc/os-release | grep -w ID | cut -d "=" -f2 |tr -d '"') 
-
-path=/efs
-
-fsid=$(aws efs describe-file-systems --region us-east-1 --query "FileSystems[*].FileSystemId" --output text)
                                                
-
-                                   
-
-function_ubuntu(){  
+function_ubuntu(){   
 
 sudo apt update -y
 sudo apt install nfs-common -y
@@ -42,11 +35,6 @@ echo "notfound"
   fi 
 
 
-sudo mkdir $path
-
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $fsid.efs.us-east-1.amazonaws.com:/ $path
-
-echo "$fsid.efs.us-east-1.amazonaws.com:/ $path nfs4 defaults,_netdev 0 0" | sudo tee -a /etc/fstab    
 
 
 
