9ce319fce24a4dae8463ceb35becf100b8247a2f
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-25 07:08:03 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -40,7 +40,6 @@ sudo mkdir $path
 
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport $fsid:/ $path
 
-echo "your text here" | sudo tee -a /etc/fstab
 echo "$fsid:/ $path nfs4 defaults,_netdev 0 0" | sudo tee -a /etc/fstab    
 
 
