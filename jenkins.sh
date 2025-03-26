626188e8b18d0a044ff35c65162d14f9598d92fe
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-26 11:41:45 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,16 @@
+
+ID=$(cat /etc/os-release | grep -w ID | cut -d "=" -f2 |tr -d '"')   
+
+if jenkins --version &> /dev/null
+
+echo "jenkins installed"
+
+
+
+else
+echo "installing jenkins"
+
+jenkins_ubuntu(){
 sudo apt update -y
 sudo apt install fontconfig openjdk-17-jre -y
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
@@ -7,5 +20,32 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install jenkins -y
+}      
+
+if [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ]; then          
+
+  jenkins_ubuntu
+
+
+elif [ "$ID" = "rhel" ] || [ "$ID" = "amzn" ]; then   
+                                                                   
+  function_redhat                                                                               
+
+else
+
+echo "notfound"            
+
+fi  
+
+ 
+
+
+
+
+
+
+
+
+
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
