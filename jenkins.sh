bf49ff6da065f673d73840210711e662abc07796
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-26 17:15:07 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -25,10 +25,9 @@ jenkins_rhel(){
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
-# Add required dependencies for the jenkins package
-sudo yum install fontconfig java-17-openjdk
-sudo yum install jenkins
+sudo yum upgrade -y
+sudo yum install fontconfig java-17-openjdk -y
+sudo yum install jenkins -y
 sudo systemctl daemon-reload
 }
 
