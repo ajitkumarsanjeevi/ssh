036fddcac08794d200dad96ff4f7f4966f8af561
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-26 09:37:37 +0530

    Update awscli.sh

--- awscli.sh
+++ awscli.sh
@@ -1,4 +1,4 @@
-sudo apt-get install curl 
+sudo apt-get install curl -y
 $ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt-get install unzip -y
 unzip awscliv2.zip
