e3c8c080c92e03fb517f93fe3aaf6f117de1db62
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-27 16:33:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ resource "aws_instance" "ec2_instances" {
   instance_type = "t2.micro"
   key_name      = "remote"
   subnet_ids      = "aws_subnet.public_subnet_1.id"
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
   vpc_security_group_ids = [aws_security_group.efs-sg.id]
 
   tags = {
