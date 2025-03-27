4f906015d8639f2b35289fb9da9b60a585cf930d
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-27 07:36:42 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -7,14 +7,7 @@ variable "egress-rules" {
   type = list(number)
   default = [22,8080,80,443,2049]
 }
-variable "ec2_instance_config" {
-  type = map(any)  
-  default = {
-    "web" = {
-      instance_type = "t2.micro"
-      ami           =  "ami-08b5b3a93ed654d19"
-      key_name      = "remote"
-      availability_zone = "us-east-1a"
-    }
-  }
-}
+variable "instances" {
+  type = list(string)
+  default = ["instance-1"]
+  
