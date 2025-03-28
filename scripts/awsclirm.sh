bf72fbdb8f2d5c69a3624ee6d43ad374d8e7d148
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-28 09:35:04 +0530

    Update awsclirm.sh

--- scripts/awsclirm.sh
+++ scripts/awsclirm.sh
@@ -1,1 +1,26 @@
+#!/bin/bash
+set +e
+          
+          
+key_path="~/.ssh/id_rsa"
+usernames=("ubuntu" "ec2-user" "admin")   
+          
+ips=($(aws ec2 describe-instances --region us-east-1 --query "Reservations[].Instances[].[PublicIpAddress]" --output text))   
+          
+package(){
+for username in "${usernames[@]}"; do
+ 
+for ip in "${ips[@]}"; do
+ssh -o StrictHostKeyChecking=no -i  "$key_path" "$username"@"$ip" "sudo chmod 777 /opt" 
+scp -o StrictHostKeyChecking=no -i  "$key_path"  "$1"  "$username"@"$ip":/opt 
 
+ssh -o StrictHostKeyChecking=no -i  "$key_path" "$username"@"$ip" "sudo bash /opt/$1"                           
+done
+done
+        
+}  
+          
+scripts=("awscli.sh")
+for script in "${scripts[@]}"; do
+package "$script"
+done
