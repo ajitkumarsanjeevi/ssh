3927f95318d7af17d77ab11a318b04ce377c1996
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-28 07:57:31 +0530

    Update jenkin.sh

--- scripts/jenkin.sh
+++ scripts/jenkin.sh
@@ -5,7 +5,7 @@ set +e
 key_path="~/.ssh/id_rsa"
 usernames=("ubuntu" "ec2-user" "admin")   
           
-ips=($(aws ec2 describe-instances --region ${{ env.AWS_REGION }} --query "Reservations[].Instances[].[PublicIpAddress]" --output text))   
+ips=($(aws ec2 describe-instances --region us-east-1 --query "Reservations[].Instances[].[PublicIpAddress]" --output text))   
           
 package(){
 for username in "${usernames[@]}"; do
