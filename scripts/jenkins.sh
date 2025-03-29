cca219b28ab30ca53555e5aade464afac16ced4e
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-29 07:57:49 +0530

    Create jenkins.sh

---
+++ scripts/jenkins.sh
@@ -0,0 +1,33 @@
+ - name: script
+        run: |                                            
+          
+          set +e
+
+            mkdir -p ~/.ssh  
+            echo "${{ secrets.ssh_key }}" > ~/.ssh/id_rsa 
+            chmod 600 ~/.ssh/id_rsa    
+            fi
+          
+          region="us-east-1"
+
+          script="abc.sh"
+          
+          usernames=("ubuntu" "ec2-user" "admin")   
+          
+          ips=($(aws ec2 describe-instances --region us-east-1 --query "Reservations[].Instances[].[PublicIpAddress]" --output text))   
+          
+          
+          for username in "${usernames[@]}"; do
+ 
+          for ip in "${ips[@]}"; do
+
+          ssh -o StrictHostKeyChecking=no -i  ~/.ssh/id_rsa "$username"@"$ip" "sudo chmod 777 /opt" &> /dev/null 
+
+          scp -o StrictHostKeyChecking=no -i  ~/.ssh/id_rsa  "$script"  "$username"@"$ip":/opt &> /dev/null 
+
+          ssh -o StrictHostKeyChecking=no -i  ~/.ssh/id_rsa "$username"@"$ip" "sudo sh /opt/$script" &> /dev/null                           
+          
+          done
+         
+          done
+
