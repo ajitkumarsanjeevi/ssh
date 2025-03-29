14e562a8020420aaef260a58c905b819c8be17b0
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-29 13:14:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {  
-region = "us-east-1    
+region = "us-east-1"    
 }
 
 
@@ -103,17 +103,17 @@ resource "aws_security_group" "efs-sg" {
 
 
 resource "aws_instance" "ec2_instances" {
-  count = length(var.instances)
+  
 
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
   key_name      = "remote"
   subnet_id     = aws_subnet.public_subnet_1.id
-  availability_zone = var.availability_zones[0]
+  availability_zone = "us-east-1a"
   vpc_security_group_ids = [aws_security_group.efs-sg.id]
 
   tags = {
-    Name = var.instances[count.index]
+    Name = "instance-1"
   }
 }
 
