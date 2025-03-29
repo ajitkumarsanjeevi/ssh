d9861e83c5e5e254ccaa56fbb4b823db1fdfc020
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-30 05:14:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-provider "aws" {  
+provider "aws" 
 region = "us-east-1"    
 }
 
@@ -104,7 +104,7 @@ resource "aws_security_group" "efs-sg" {
 
 resource "aws_instance" "ec2_instances" {
   
-  count = length(var.instances)
+
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
   key_name      = "remote"
@@ -113,7 +113,7 @@ resource "aws_instance" "ec2_instances" {
   vpc_security_group_ids = [aws_security_group.efs-sg.id]
 
   tags = {
-    Name = var.instances[count.index]
+    Name = "instance-1"
   }
 }
 
