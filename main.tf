8d5f08f175d0fdba95d4c859c4fdc0e91b8efcc8
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-30 06:54:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -104,7 +104,7 @@ resource "aws_security_group" "efs-sg" {
 
 resource "aws_instance" "ec2_instances" {
 
-  count         =     length(var.ec2_instances)
+  count         = length(var.ec2_instances)
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
   key_name      = "remote"
