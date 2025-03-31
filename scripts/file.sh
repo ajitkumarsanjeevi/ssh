fb5d9d6d9817d7541477965f6778134c3126a399
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-31 16:01:47 +0530

    Update file.sh

--- scripts/file.sh
+++ scripts/file.sh
@@ -1,1 +1,27 @@
+ - name: script
+        run: |
+           set +e
+          
+          
+            key_path="~/.ssh/id_rsa"
+          
+          
+            
+            usernames=("ubuntu" "ec2-user" "admin")   
+          
+            ips=($(aws ec2 describe-instances --region ${{ env.Region }} --query "Reservations[].Instances[].[PublicIpAddress]" --output text))   
+          
+          
+             for username in "${usernames[@]}"; do
+ 
+             for ip in "${ips[@]}"; do
+
+             
+             ssh -o StrictHostKeyChecking=no -i  "$key_path" "$username"@"$ip" "sudo chmod 777 /opt/" &> /dev/null
+
+             scp -o StrictHostKeyChecking=no -i  "$key_path"  "$1" "$username"@"$ip":/opt &> /dev/null
+              
+             ssh -o StrictHostKeyChecking=no -i  "$key_path" "$username"@"$ip" "sudo bash /opt/$1" &> /dev/null
+
+             
 
