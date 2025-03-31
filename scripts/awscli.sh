216c0e472bf88d87ba7e7cef356011f39ed732ea
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-03-31 18:55:33 +0530

    Update awscli.sh

--- scripts/awscli.sh
+++ scripts/awscli.sh
@@ -7,7 +7,7 @@
           
 
           
-          packages(){
+          script="awscli.sh"
           usernames=("ubuntu" "ec2-user" "admin")   
           
           ips=($(aws ec2 describe-instances --region us-east-1 --query "Reservations[].Instances[].[PublicIpAddress]" --output text))   
@@ -19,17 +19,14 @@
 
           ssh -o StrictHostKeyChecking=no -i  ~/.ssh/id_rsa "$username"@"$ip" "sudo chmod 777 /opt" &> /dev/null 
 
-          scp -o StrictHostKeyChecking=no -i  ~/.ssh/id_rsa  "$1"  "$username"@"$ip":/opt &> /dev/null 
+          scp -o StrictHostKeyChecking=no -i  ~/.ssh/id_rsa  "$script"  "$username"@"$ip":/opt &> /dev/null 
 
-          ssh -o StrictHostKeyChecking=no -i  ~/.ssh/id_rsa "$username"@"$ip" "sudo bash /opt/$1"                         
+          ssh -o StrictHostKeyChecking=no -i  ~/.ssh/id_rsa "$username"@"$ip" "sudo bash /opt/$script"                         
           
           done
          
           done
           }
 
-          scripts=("awscli.sh" "jenkins.sh") 
-          for script in "${scripts[@]}";do
-          packages "$script"
-          done
+          
           
