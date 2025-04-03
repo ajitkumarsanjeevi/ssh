be96889df609ac1b3d57d6ff88fb22f9b7b4f211
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-03 06:29:21 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" { 
     bucket = "my-ansible-iam"
-    key    = "my-terraform-state"
+    key    = "statefile/terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "my-terraform-lock" 
   }
