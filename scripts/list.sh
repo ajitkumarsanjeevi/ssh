bb2b0439abaccbef83cda86c9dc7fe55154b6bd5
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-06 10:53:30 +0530

    Update list.sh

--- scripts/list.sh
+++ scripts/list.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-aws configure set aws-access-key-id:  $AWS_ACCESS_KEY_ID
-aws configure set aws-secret-access-key: $AWS_SECRET_ACCESS_KEY
-aws configure set aws-region:  $AWS_DEFAULT_REGION
+aws configure set aws-access-key-id $AWS_ACCESS_KEY_ID
+aws configure set aws-secret-access-key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region $AWS_DEFAULT_REGION
 aws s3 ls
 
