ddd0b465f73e4ed43cbf2cdba3567becf8bcf7bd
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-10 09:49:56 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -18,7 +18,11 @@ variable "egress-rules" {
   type = list(number)
   default = [22,8080,80,443,2049]
 }
+variable "subnet_ids" {
+  type    = list(string)
 
+  default = ["subnet-0c847ba2112e16310", "subnet-07d1742f3544a89c7"]
+}
 
 
 
