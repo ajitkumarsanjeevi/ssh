e132a614d6361e75aa931153e86aa0fa70188622
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-17 07:37:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "public_subnet_1" {   
+resource "aws_subnet" "public_subnet_1" {    
   vpc_id                  = aws_vpc.main.id      
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = var.is_enabled
