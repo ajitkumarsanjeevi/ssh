1c6643ce07aee3116f00c6ee71b286d8fe3d30d0
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-18 08:16:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -125,3 +125,17 @@ resource "aws_instance" "example" {
   }
 }
 
+
+resource "aws_efs_file_system" "efs" {
+  creation_token = "my-efs"
+  performance_mode = "generalPurpose"
+  tags = {
+    Name = "my-efs"
+  }
+}
+
+resource "aws_efs_mount_target" "efs_mount_target" {
+  file_system_id  = aws_efs_file_system.efs.id
+  subnet_id       = var.subnet_id
+  security_groups = [aws_security_group.efs_sg.id]
+}
