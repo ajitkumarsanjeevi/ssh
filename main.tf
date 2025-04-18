7298fadc3c293cc8bb8996e4c84b4918c828fe10
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-18 08:13:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -112,4 +112,16 @@ resource "aws_instance" "example" {
     Name = "instance-1"
   }
 }
+resource "aws_instance" "example" {
+  ami                = "ami-0e35ddab05955cf57"
+  instance_type      = "t2.micro"
+  key_name           = "splunk"
+  subnet_id          = aws_subnet.public_subnet_1.id
+  vpc_security_group_ids = [aws_security_group.efs-sg.id]
+  availability_zone  = "ap-south-1a"
+
+  tags = {
+    Name = "instance-2"
+  }
+}
 
