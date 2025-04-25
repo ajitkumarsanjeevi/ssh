0d4c2e2426f1d8d87f9668023fc0cd3672be62c2
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-25 11:02:24 +0530

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /opt
 
+sudo yum install wget -y
+
 sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
 
 sudo tar xf node_exporter-1.9.1.linux-amd64.tar.gz
