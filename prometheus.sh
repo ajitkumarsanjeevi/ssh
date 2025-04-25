9356d9f669971f48049abdc2c022967781a32f95
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-25 10:18:18 +0530

    Create prometheus.sh

---
+++ prometheus.sh
@@ -0,0 +1,12 @@
+sudo cd /opt
+
+sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
+
+sudo tar xf node_exporter-1.9.1.linux-amd64.tar.gz
+
+sudo mv node_exporter-1.9.1.linux-amd64.tar.gz node_exporter
+
+sudo cd node_exporter
+
+sudo ./node_exporter
+
