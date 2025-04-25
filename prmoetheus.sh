84300807abf5932a456a137ce631c83a269edc74
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-25 12:30:22 +0530

    Create prmoetheus.sh

---
+++ prmoetheus.sh
@@ -0,0 +1,10 @@
+cd /opt
+
+sudo yum install nginx -y
+
+sudo wget https://github.com/prometheus/prometheus/releases/download/v3.3.0/prometheus-3.3.0.linux-amd64.tar.gz
+
+sudo tar xf prometheus-3.3.0.linux-amd64.tar.gz 
+
+sudo mv  prometheus-3.3.0.linux-amd64 prometehus
+
