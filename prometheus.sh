6e57f091ede5b3e59b891bacd86dbd8247933a6d
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-25 16:30:39 +0530

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -1,7 +1,5 @@
 cd /opt
 
-sudo apt install wget -y
-
 sudo wget https://github.com/prometheus/prometheus/releases/download/v3.3.0/prometheus-3.3.0.linux-amd64.tar.gz
 
 sudo tar xf prometheus-3.3.0.linux-amd64.tar.gz 
