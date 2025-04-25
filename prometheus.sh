723eed39f6520976d971034e0dba42aa280de727
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-25 12:51:08 +0530

    Create prometheus.sh

---
+++ prometheus.sh
@@ -0,0 +1,9 @@
+cd /opt
+
+sudo yum install wget -y
+
+sudo wget https://github.com/prometheus/prometheus/releases/download/v3.3.0/prometheus-3.3.0.linux-amd64.tar.gz
+
+sudo tar xf prometheus-3.3.0.linux-amd64.tar.gz 
+
+sudo mv  prometheus-3.3.0.linux-amd64 prometehus
