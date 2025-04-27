474a5b19fd7d3a9b7cb469b12a97f3e977fb05b4
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-27 11:45:16 +0530

    Update prmoetheus.sh

--- prmoetheus.sh
+++ prmoetheus.sh
@@ -1,3 +1,11 @@
+# Set your Prometheus directory path
+PROMETHEUS_DIR="/opt/prometheus"
+
+# Check if the directory exists
+if [ -d "$PROMETHEUS_DIR" ]; then
+  echo "Prometheus directory exists at $PROMETHEUS_DIR"
+else
+  
 cd /opt
 
 sudo apt install wget -y
@@ -7,4 +15,6 @@ sudo wget https://github.com/prometheus/prometheus/releases/download/v3.3.0/prom
 sudo tar xf prometheus-3.3.0.linux-amd64.tar.gz 
 
 sudo mv  prometheus-3.3.0.linux-amd64 prometehus
+
+fi
 
