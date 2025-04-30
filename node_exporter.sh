5d48aa356db3b65392292104cf2b23e5532629c0
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-30 17:08:19 +0530

    Update node_exporter.sh

--- node_exporter.sh
+++ node_exporter.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-PROMETHEUS_DIR="/opt/node_exporter"
+node_exporter="/opt/node_exporter"
 
 # Check if the directory exists
 if [ -d "$node_exporter" ]; then
-  echo "Prometheus directory exists at $PROMETHEUS_DIR"
+  echo "Prometheus directory exists at $node_exporter"
 else
 cd /opt
 
-sudo apt install wget -y 
+sudo apt install wget -y
 
 sudo wget https://github.com/prometheus/node_exporter/releases/download/v1.9.1/node_exporter-1.9.1.linux-amd64.tar.gz
 
@@ -18,5 +18,6 @@ sudo mv node_exporter-1.9.1.linux-amd64 node_exporter
 
 cd node_exporter
 
-./node_exporter 
+./node_exporter
 
+fi
