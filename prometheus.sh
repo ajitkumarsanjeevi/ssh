bad6ace6b27de39cfc2561de857415c2f8ade752
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-04-30 10:45:37 +0530

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -1,3 +1,10 @@
+PROMETHEUS_DIR="/opt/prometheus"
+
+# Check if the directory exists
+if [ -d "$PROMETHEUS_DIR" ]; then
+  echo "Prometheus directory exists at $PROMETHEUS_DIR"
+else
+
 cd /opt
 
 sudo wget https://github.com/prometheus/prometheus/releases/download/v3.3.0/prometheus-3.3.0.linux-amd64.tar.gz
