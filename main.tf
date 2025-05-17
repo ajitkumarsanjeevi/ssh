93865e9bbf7f29649de7e3193f8f90b949cc0287
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-05-17 22:32:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -139,6 +139,6 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.main.id
   service_name      = "com.amazonaws.ap-south-1.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids   = [aws_route_table.example.private_rt.id]
+  route_table_ids   = [aws_route_table.private_rt.id]
 }
 
