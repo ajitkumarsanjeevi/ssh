22c76103229e79206714da6c7227f57bdf389eb7
ajitkumarsanjeevi <sajitkumar8523@gmail.COM>
2025-05-26 19:37:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,7 +27,6 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = var.is_enabled
   availability_zone       = var.availability_zones[1]
 
   tags = {
